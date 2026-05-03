fd1312368f5bf41aa6d188d40d2ba4220f9f9742
Sushant-Coder-01 <sushantmangore45@gmail.com>
2026-05-03 20:07:34 +0530

    Understading middleware indepth

--- server/src/app.js
+++ server/src/app.js
@@ -2,31 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.use("/user", [
-  (req, res, next) => {
-    // Route handler 1
-    console.log("This is the user route handler 1");
-    next();
-  },
-  (req, res, next) => {
-    // Route handler 2
-    console.log("This is the user route handler 2");
-    // res.send("This is route handler 2");
-    next();
-  },
-  (req, res, next) => {
-    // Route handler 2
-    console.log("This is the user route handler 3");
-    // res.send("This is route handler 3");
-    next();
-  },
-  (req, res, next) => {
-    // Route handler 2
-    console.log("This is the user route handler 4");
-    // res.send("This is route handler 4");
-    next();
-  },
-]);
+app.use("/user", (req, res, next) => {
+  // Route handler 2
+  console.log("This is the user route handler 2");
+  // res.send("This is the user route handler 2");
+  next();
+});
+
+app.use("/user", (req, res, next) => {
+  // Route handler 1
+  console.log("This is the user route handler 1");
+  next();
+});
 
 app.listen("3000", () => {
   console.log("Server is started successfully on port 3000...");

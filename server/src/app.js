86879fe885c17997457285d141ce52ffc52233fe
Sushant-Coder-01 <sushantmangore45@gmail.com>
2026-05-03 17:55:30 +0530

    Add nodemon for development and update main entry point in package.json; enhance app.js with user routes and home page response

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,41 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello this is home page!");
-});
-
-app.use("/test", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Hello test!");
 });
 
 app.listen("3000", () => {
   console.log("Server is started successfully on port 3000...");
+});
+
+app.get("/user", (req, res) => {
+  const user = {
+    name: "Sushant Mangore",
+    age: 24,
+    profession: "Software Engineer",
+  };
+  res.send(user);
+});
+
+app.post("/user", (req, res) => {
+  const user = {
+    name: "Sushant Mangore",
+    age: 24,
+    profession: "Software Engineer",
+  };
+  res.send(`User ${user.name} created successfully!`);
+});
+
+app.delete("/user", (req, res) => {
+  const user = {
+    name: "Sushant Mangore",
+    age: 24,
+    profession: "Software Engineer",
+  };
+  res.send(`User ${user.name} deleted successfully!`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello this is home page!");
 });

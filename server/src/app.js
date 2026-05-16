4d59ffae346dcf011c14e98bb6b4873292613567
Sushant-Coder-01 <sushantmangore45@gmail.com>
2026-05-16 17:26:02 +0530

    Implement user retrieval and feed APIs

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const user = new User(userData);
+  const user = User(req.body);
 
   try {
     await user.save();
@@ -15,6 +15,37 @@ app.post("/signup", async (req, res) => {
     res.send("User Added Successfully!");
   } catch (error) {
     res.status(400).send("Error saving the user: " + error.message);
+  }
+});
+
+// Get user from the email
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+
+    if (user.length === 0) {
+      res.status(404).send("User not found!");
+    }
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Error while get the user: ", error);
+  }
+});
+
+// Feed API, get all the users from the database.
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    if (users.length === 0) {
+      res.status(404).send("No user exists!");
+    }
+
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Error while get all the users: ", error);
   }
 });
 
